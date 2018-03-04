c904f48333e38c598abdcc2e1e2b4871f8910b7f
Tim D. Smith <git@tim-smith.us>
2018-03-03 16:41:41 -0800

    Fix test for _dates_to_archive

--- yvrtransit/test/test_fetch.py
+++ yvrtransit/test/test_fetch.py
@@ -7,6 +7,6 @@ class TestFetch:
     def test_dates_to_archive(self):
         today = dt.date(2005, 5, 5)
         past = ["2005-05-04T05:05_stub"]
-        present = ["2005_05_05T00:00_stub"]
+        present = ["2005-05-05T00:00_stub"]
         to_archive = _dates_to_archive(past+present, today)
         assert to_archive == ["2005-05-04"]
